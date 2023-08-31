f0e95be0bf7dcf1b439ea376a8f5eac223be03ec
= <=>
2023-08-31 07:14:02 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,57 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
-const App = () => {
+const items = [
+  {
+    name: "Apple",
+    price: 20
+  },
+  {
+    name: "Banana",
+    price: 10
+  },
+  {
+    name: "Neutella",
+    price: 30
+  },
+  {
+    name: "Razor",
+    price: 5
+  },
+  {
+    name: "CornFlakes",
+    price: 15
+  },
+  {
+    name: "Sound Bar",
+    price: 50
+  },
+  {
+    name: "Iphone",
+    price: 80
+  }
+];
+export default function App() {
+  let [budget, setBudget] = useState("");
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      <p>
+        <b>Enter your budget to check available items:</b>
+      </p>
+      <input type="number" onChange={(e)=> setBudget(e.target.value)} />
+      <p>
+        <b>Items you can buy are in Green color</b>
+      </p>
+      <div>
+      {items.map((item) => (
+        <div className="items">
+          <p>{item.name}</p>
+          {budget>= item.price ? (<p style={{color: "green"}}>{item.price}</p>) : (<p style={{color: "red"}}>{item.price}</p>)}
+        </div>
+      ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
